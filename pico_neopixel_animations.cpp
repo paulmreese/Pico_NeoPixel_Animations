58e98c6d7c74308f39267b5f4484264be652b0e0
paulmreese <paul.m.reese@gmail.com>
2024-01-10 13:34:52 -0500

    Update initialization documentation

--- pico_neopixel_animations.cpp
+++ pico_neopixel_animations.cpp
@@ -19,16 +19,13 @@ NeoPixelStrip::NeoPixelStrip(
     // this is a member initializer list. It allows us to construct strip 
     // without having to copy anything.
     // Argument 1 = Number of pixels in NeoPixel strip
-    // Argument 2 = Arduino pin number (most are valid)
+    // Argument 2 = Pico pin number (most are valid)
     // Argument 3 = Pixel type flags, add together as needed:
     //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
     //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
     //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
     //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
     //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-    //(pixelOrderString == "default") ? std::string("default") : std::string("A string!") //Make it an array to iterate over
-    //TODO: Make setPixelOrder function checking for blank string or breaking
-    //      the string into the appropriate number of pieces
 {
     // INITIALIZE NeoPixel strip
     interpretPixelOrder(pixelOrderString);
